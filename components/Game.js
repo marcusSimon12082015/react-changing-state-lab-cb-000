ed26ef3b26b8c9965052cf250fcfc8592ddb9713
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-13 14:22:51 +0000

    Automatically backed up by Learn

--- components/Game.js
+++ components/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Board from './Board';
 import Status from './Status';
+import Field from './Field';
 import solutions from './solutions';
 
 export default class Game extends React.Component {
@@ -16,26 +17,46 @@ export default class Game extends React.Component {
   }
 
   handleReset (ev) {
+    ev.preventDefault();
+    this.setState({board:this.state.board.fill(null)});
   }
 
   handleClick (i, ev) {
+    ev.preventDefault();
+    var player = 'X';
+    if (this.state.turn === 'X') {
+      player = 'O';
+    }
+    this.setState(board => {
+      const newBoard = [...this.state.board];
+      newBoard[i] = this.state.turn;
+      return {board:newBoard};
+    });
+    this.setState({turn:player});
   }
 
-  getWinner () {
-    return this.state.turn;
+  getWinner(){
+    solutions.some(function(combo){
+      if(this.state.board[combo[0]] !== null && this.state.board[combo[0]] === this.state.board[combo[1]] && this.state.board[combo[1]] === this.state.board[combo[2]]){
+        this.setState({winner:this.state.board[combo[0]]});
+        return true;
+      }
+    });
   }
 
   isComplete () {
-
+    if(getWinner()){
+      return this.state.turn;
+    }
   }
 
   render () {
+    let className = 'game';
     return (
-      <div>
-      <Board />
-      if(this.isComplete){
-        <Status winner={this.getWinner}/>
-      }
+      <div className={className}>
+      <Board board={this.state.board} onClick={this.handleClick}/>
+      {this.isComplete && <Status winner={this.getWinner} />}
+      <button onClick={this.handleReset} className='game__reset'>Reset</button>
       </div>
     );
   }
